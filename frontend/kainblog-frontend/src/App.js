de950b488aae4a0f366fa0d3479e86790c26d582
Peter Gottlieb <gotped17@htl-kaindorf.at>
2021-11-30 17:18:12 +0100

    included rss-reader functionality

--- frontend/kainblog-frontend/src/App.js
+++ frontend/kainblog-frontend/src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import Artikel from  './artikel';
+import React, {useState} from 'react';
+//var XMLParser = require('react-xml-parser');
+
 function App() {
+  const RSS_URL = `https://www.kleinezeitung.at/rss/sport`;
+  const [items, setItems] = useState([]);
+
+  const getRss = async (e) => {
+    const res = await fetch(RSS_URL);
+    const  contents  = await res.text();
+    
+    //var parser = new XMLParser();
+    //const feed = parser.parseFromString(contents);
+    //const xml = feed.querySelectorAll("item");
+    const feed = new window.DOMParser().parseFromString(contents, "text/xml");
+    const items = feed.querySelectorAll("item");
+    const feedItems = [...items].map((el) => ({
+      link: el.querySelector("link").innerHTML,
+      title: el.querySelector("title").innerHTML,
+      description: el.querySelector("description").innerHTML,
+      guid: el.querySelector("guid").innerHTML,
+      enclosure: el.querySelector("enclosure").getAttribute("url")
+    }));
+    setItems(feedItems);
+  };
   return (
-    <div className="App">
+    <div className="App" onLoad={getRss}>
       <header className="App-header">
 
       </header>
       <div className="filterContainer">xcv</div>
 
       <div className="artikelList">
-        <Artikel img={logo} link="www.google.com" title="test123" desc="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."/>
-        <Artikel title="test456" desc="yxcybcv"/>
+        {
+          items.map(item => <Artikel img={item.enclosure} title={item.title} desc={item.description} link={item.link}  />)
+        }
+        <span style={{display: 'none'}}>
+        <Artikel img={logo}/>
+        </span>
+        
       </div>
       
     </div>
