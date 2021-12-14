9926214abd78578e0af1fef02c22c15210c07e79
Peter Gottlieb <gotped17@htl-kaindorf.at>
2021-12-14 15:03:31 +0100

    Changed to own rss

--- frontend/kainblog-frontend/src/App.js
+++ frontend/kainblog-frontend/src/App.js
@@ -5,27 +5,30 @@ import React, {useState} from 'react';
 //var XMLParser = require('react-xml-parser');
 
 function App() {
-  const RSS_URL = `https://www.kleinezeitung.at/rss/sport`;
+  const RSS_URL = `http://192.168.0.190:8080/rss/feed/`;
   const [items, setItems] = useState([]);
 
   const getRss = async (e) => {
     const res = await fetch(RSS_URL);
     const  contents  = await res.text();
-    
+    console.log(contents);
     //var parser = new XMLParser();
     //const feed = parser.parseFromString(contents);
     //const xml = feed.querySelectorAll("item");
     const feed = new window.DOMParser().parseFromString(contents, "text/xml");
     const items = feed.querySelectorAll("item");
+
     const feedItems = [...items].map((el) => ({
       link: el.querySelector("link").innerHTML,
       title: el.querySelector("title").innerHTML,
       description: el.querySelector("description").innerHTML,
       guid: el.querySelector("guid").innerHTML,
-      enclosure: el.querySelector("enclosure").getAttribute("url")
+      enclosure: el.querySelector("enclosure").getAttribute("url"),
+      categories: el.querySelector("category").innerHTML
     }));
     setItems(feedItems);
   };
+  
   return (
     <div className="App" onLoad={getRss}>
       <header className="App-header">
@@ -35,7 +38,7 @@ function App() {
 
       <div className="artikelList">
         {
-          items.map(item => <Artikel img={item.enclosure} title={item.title} desc={item.description} link={item.link}  />)
+          items.map(item => <Artikel img={item.enclosure} title={item.title} desc={item.description} link={item.link} tags={item.categories} />)
         }
         <span style={{display: 'none'}}>
         <Artikel img={logo}/>
--- frontend/kainblog-frontend/src/artikel.js
+++ frontend/kainblog-frontend/src/artikel.js
@@ -1,10 +1,22 @@
 import './artikel.css';
 
 function Artikel(props){
+    //var categories = props.categories;
+    var categories = String(props.tags).split('/');
+    var color = '#72326c';
+    if(categories.includes('Informatik'))
+        color = '#5d8ec6';
+    else if(categories.includes('Automatisierung'))
+        color = '#b5213a';
+    else if(categories.includes('Mechatronik'))
+        color = '#cb9e10';
+    else if(categories.includes('Robotik'))
+        color = '#008000';
+    
     return (
-        <a href={props.link}>
-            <div className="artikelContainer">
-        <div  className="artikelHeadline">
+        <a className='articleLink' href={props.link}>
+            <div className="artikelContainer" >
+        <div  className="artikelHeadline" style={{backgroundColor: color}}>
             <h2><a href={props.link}> {props.title} </a></h2>
         </div>
         <div className="descContainer">
